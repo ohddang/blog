d98b7dd807b1cb255c0f30b38b5f9bc8f186cb09
ohddang <ohddang509@gmail.com>
2023-12-30 21:13:06 +0900

    프로젝트 리스트 -> 롤링 형식으로 변경

--- src/dice.js
+++ src/dice.js
@@ -1,26 +1,27 @@
 const dice = document.getElementById("dice");
-const face = document.getElementById("face");
 const drag_panel = document.getElementById("drag_panel");
 
-const face1 = document.getElementById("face1");
-const face2 = document.getElementById("face2");
-const face3 = document.getElementById("face3");
-const face4 = document.getElementById("face4");
-
-const square = document.getElementById("square");
-const game = document.getElementById("game");
+const body = document.querySelector("body");
+const contents = document.querySelector(".contents");
 
 let isDragging = false;
 let startMouseX = 0;
 let startMouseY = 0;
 let currentRotationX = -45;
 let currentRotationY = 45;
+let currentPositionX = 0;
+let currentPositionY = 0;
 
 dice.addEventListener("mousedown", startDrag);
 drag_panel.addEventListener("mousedown", startDrag);
 document.addEventListener("mouseup", stopDrag);
+window.addEventListener("resize", onResizeBrowser);
 
-dice.style.transform = `rotateX(${currentRotationX}deg) rotateY(${currentRotationY}deg)`;
+function onResizeBrowser(event) {
+  body.style.height = `${window.outerHeight}px`;
+}
+
+setRandomPositionDice();
 
 function startDrag(event) {
   isDragging = true;
@@ -40,7 +41,7 @@ function dragDice(event) {
     currentRotationX %= 360;
     currentRotationY %= 360;
 
-    dice.style.transform = `rotateX(${currentRotationX}deg) rotateY(${currentRotationY}deg)`;
+    rotateDice(currentRotationX, currentRotationY);
 
     updateScreen();
 
@@ -52,9 +53,49 @@ function dragDice(event) {
 function stopDrag() {
   if (isDragging) {
     isDragging = false;
+    contents.style.backgroundColor = `rgba(${currentRotationX % 256}, ${
+      currentRotationY % 256
+    }, ${(currentRotationX + currentRotationY) % 256}, 0.2)`;
   }
 }
 
 function updateScreen() {
   dice.getBoundingClientRect();
+}
+
+export function setRandomPositionDice() {
+  const area_x = dice.parentElement.offsetWidth * 0.6;
+  const area_y = dice.parentElement.offsetHeight * 0.6;
+
+  const new_x =
+    Math.floor(Math.random() * area_x) + dice.parentElement.offsetWidth * 0.2;
+  const new_y =
+    Math.floor(Math.random() * area_y) + dice.parentElement.offsetHeight * 0.1;
+  dice.style.transform = `translateX(${new_x}px) translateY(${new_y}px)`;
+  translateDice(new_x, new_y);
+}
+
+setInterval(() => {
+  currentRotationX += 3;
+  currentRotationY += 3;
+  currentRotationX %= 360;
+  currentRotationY %= 360;
+
+  rotateDice(currentRotationX, currentRotationY);
+
+  contents.style.backgroundColor = `rgba(${currentRotationX % 256}, ${
+    currentRotationY % 256
+  }, ${(currentRotationX + currentRotationY) % 256}, 0.2)`;
+}, 500);
+
+function translateDice(newX, newY) {
+  currentPositionX = newX;
+  currentPositionY = newY;
+  dice.style.transform = `translateX(${currentPositionX}px) translateY(${currentPositionY}px) rotateX(${currentRotationX}deg) rotateY(${currentRotationY}deg)`;
+}
+
+function rotateDice(newX, newY) {
+  currentRotationX = newX;
+  currentRotationY = newY;
+  dice.style.transform = `translateX(${currentPositionX}px) translateY(${currentPositionY}px) rotateX(${currentRotationX}deg) rotateY(${currentRotationY}deg)`;
 }
--- src/project.js
+++ src/project.js
@@ -1,15 +1,18 @@
+import { setRandomPositionDice } from "./dice.js";
+
 const skill_list = document.querySelector(".skill_list");
+const skill_list_dummy = document.querySelector(".skill_list_dummy");
 const project_list = document.querySelector(".project_list");
 
 const image_root = "/assets/logo";
 
 const css = "css.png";
-const js = "js.png";
+const javascript = "javascript.png";
 const nextjs = "nextjs.png";
-const react_query = "react-query.png";
+const react_query = "reactQuery.png";
 const react = "react.png";
 const redux = "redux.png";
-const styled_components = "styled-components.png";
+const styled_components = "styledComponents.png";
 const tailwind = "tailwind.png";
 const threejs = "threejs.png";
 const typescript = "typescript.png";
@@ -17,7 +20,7 @@ const zustand = "zustand.png";
 
 var logos = [
   css,
-  js,
+  javascript,
   nextjs,
   react_query,
   react,
@@ -29,14 +32,18 @@ var logos = [
   zustand,
 ];
 
-// project info
+let projectDatas = [];
+
 fetch("json/project.json")
   .then((response) => response.json())
   .then((items) => {
-    console.log(items);
+    projectDatas = items;
+
     items.map((item) => {
       const project_item = document.createElement("li");
       project_list.appendChild(project_item);
+
+      project_item.setAttribute("id", item.id);
       project_item.classList.add("project_item");
 
       const project_title_dummy = document.createElement("div");
@@ -54,32 +61,63 @@ fetch("json/project.json")
       project_image.src = item.thumb_url;
 
       project_item.addEventListener("mouseover", (event) =>
-        onMouseover(event, project_title)
+        onMouseoverProject(event, project_title)
       );
 
       project_item.addEventListener("mouseout", (event) =>
-        onMouseout(event, project_title)
+        onMouseoutProject(event, project_title)
+      );
+
+      project_item.addEventListener("click", (event) =>
+        onClickProject(event, item.id)
       );
     });
   });
 
-function onMouseover(event, title) {
-  console.log(event);
-  title.classList.add("project_title_slide_up");
-}
-
-function onMouseout(event, title) {
-  title.classList.remove("project_title_slide_up");
-}
-
 // logo
 logos.map((logo) => {
   const skill_item = document.createElement("li");
-  skill_list.appendChild(skill_item);
-  skill_item.setAttribute("id", logo);
+  skill_list_dummy.appendChild(skill_item);
+
+  skill_item.setAttribute("id", logo.split(".")[0]);
   skill_item.classList.add("skill_item");
   const skill_image = document.createElement("img");
   skill_item.appendChild(skill_image);
   skill_image.classList.add("skill_image");
   skill_image.src = `${image_root}/${logo}`;
 });
+
+function onMouseoverProject(event, title) {
+  title.classList.add("project_title_slide_up");
+}
+
+function onMouseoutProject(event, title) {
+  title.classList.remove("project_title_slide_up");
+}
+
+function onClickProject(event, id) {
+  projectDatas.map((item) => {
+    if (item.id === id) {
+      displayUseSkill(item.skills);
+    }
+  });
+
+  displayPreview();
+
+  setRandomPositionDice();
+}
+
+function displayUseSkill(skills) {
+  while (skill_list.firstChild) {
+    skill_list.removeChild(skill_list.firstChild);
+  }
+
+  console.log(skills);
+  skill_list_dummy.childNodes.forEach((node) => {
+    if (skills.includes(node.id)) {
+      skill_list.appendChild(node.cloneNode(true));
+    }
+  });
+}
+
+function displayPreview() {}
