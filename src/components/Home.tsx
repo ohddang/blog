84a75257188d0207da29fdfa598a7a0060b3f2af
DESKTOP-TFLRN4C\GON <ohddang509@gmail.com>
2024-06-14 16:48:56 +0900

    카드 effect 적용

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -2,41 +2,43 @@ import Card from "./ui/Card";
 import TypingText from "./ui/TypingText";
 
 const frontend = [
-  "logo/javascript.png",
-  "logo/typescript.png",
-  "logo/react.png",
-  "logo/nextjs.png",
-  "logo/tailwind.png",
-  "logo/styledComponents.png",
-  "logo/reactQuery.png",
-  "logo/redux.png",
-  "logo/zustand.png",
+  { name: "javascript", url: "logo/javascript.png" },
+  { name: "typescript", url: "logo/typescript.png" },
+  { name: "react", url: "logo/react.png" },
+  { name: "nextjs", url: "logo/nextjs.png" },
+  { name: "tailwind", url: "logo/tailwind.png" },
+  { name: "styledComponents", url: "logo/styledComponents.png" },
+  { name: "reactQuery", url: "logo/reactQuery.png" },
+  { name: "redux", url: "logo/redux.png" },
+  { name: "zustand", url: "logo/zustand.png" },
 ];
 
 const backend = [
-  "logo/nodejs.svg",
-  "logo/nestjs.svg",
-  "logo/aws.svg",
-  "logo/mysql.png",
-  "logo/mongodb.png",
-  "logo/prisma.svg",
-  "logo/typeorm.svg",
+  { name: "", url: "logo/nodejs.svg" },
+  { name: "", url: "logo/nestjs.svg" },
+  { name: "", url: "logo/mysql.png" },
+  { name: "", url: "logo/mongodb.png" },
+  { name: "", url: "logo/prisma.svg" },
+  { name: "", url: "logo/typeorm.svg" },
 ];
 
 const intro = `안녕하세요. 웹 개발자 오영곤입니다. <br>저는 프론트엔드로 웹 개발을 시작하였으며<br>이후 짧은 기간동안 다양한 경험을 통해 새로운 환경에서도 빨리 적응할 수 있는 역량을 키웠습니다. <br>추후에는 프론트엔드, 백엔드, 인프라를 아우르는 풀스택 개발자가 되기 위해 노력하고 있습니다.`;
 
-const etc = ["logo/docker.png"];
+const etc = [
+  { name: "", url: "logo/aws.svg" },
+  { name: "", url: "logo/docker.png" },
+];
 
 const contentComponent = () => {
   return (
     <div className="w-full h-full flex flex-row justify-around items-center">
       <img
-        className="h-4/6 cursor-pointer"
+        className="h-1/2 cursor-pointer"
         src="logo/github.png"
         onClick={() => window.open("https://github.com/ohddang/", "_blank")}
       />
       <img
-        className="h-4/6 cursor-pointer"
+        className="h-1/2 cursor-pointer"
         src="logo/email.svg"
         onClick={() => (window.location.href = "mailto:your-email@example.com")}
       />
@@ -52,27 +54,23 @@ export default function Home() {
           <TypingText text={intro} />
           <Card url="images/profile.jpg" size="lg" children={contentComponent()} />
         </div>
-        <div className="flex flex-col gap-5">
+        <div className="flex flex-col gap-10">
           <div className="text-3xl font-bold">FrontEnd</div>
           <div className="flex flex-row gap-5">
-            {frontend.map((src, i) => {
-              return <img key={i} className="w-14 h-14" src={src} />;
+            {frontend.map((item, i) => {
+              return <Card url={item.url} size="sm" isSquare={true} />;
             })}
           </div>
-        </div>
-        <div className="flex flex-col gap-5">
           <div className="text-3xl font-bold">BackEnd</div>
           <div className="flex flex-row gap-5">
-            {backend.map((src, i) => {
-              return <img key={i} className="w-14 h-14" src={src} />;
+            {backend.map((item, i) => {
+              return <Card url={item.url} size="sm" isSquare={true} />;
             })}
           </div>
-        </div>
-        <div className="flex flex-col gap-5">
           <div className="text-3xl font-bold">ETC</div>
           <div className="flex flex-row gap-5">
-            {etc.map((src, i) => {
-              return <img key={i} className="w-14 h-14" src={src} />;
+            {etc.map((item, i) => {
+              return <Card url={item.url} size="sm" isSquare={true} />;
             })}
           </div>
         </div>
--- src/components/ui/Card.tsx
+++ src/components/ui/Card.tsx
@@ -3,16 +3,38 @@ import { useEffect, useRef, useState } from "react";
 interface CardProps {
   url: string;
   size: "sm" | "md" | "lg";
+  isSquare?: boolean;
   children?: React.ReactNode;
 }
 
 export default function Card(props: CardProps) {
   const cardRef = useRef<HTMLImageElement>(null);
-  const [cardScale, setCardScale] = useState<number>(0);
+  const [cardScale, setCardScale] = useState<string>("scale-0");
   const [bgColor, setBgColor] = useState<string>("");
 
-  const width = props.size === "sm" ? "w-72" : props.size === "md" ? "w-1/2" : "w-lg";
-  const height = props.size === "sm" ? "h-96" : props.size === "md" ? "h-5/6" : "h-lg";
+  let width = props.isSquare
+    ? props.size === "sm"
+      ? "w-square-sm"
+      : props.size === "md"
+      ? "w-sqaure-md"
+      : "w-square-lg"
+    : props.size === "sm"
+    ? "w-20"
+    : props.size === "md"
+    ? "w-1/2"
+    : "w-rect-lg";
+  let height = props.isSquare
+    ? props.size === "sm"
+      ? "h-square-sm"
+      : props.size === "md"
+      ? "h-square-md"
+      : "h-square-lg"
+    : props.size === "sm"
+    ? "h-32"
+    : props.size === "md"
+    ? "h-5/6"
+    : "h-rect-lg";
+
   const pt = props.size === "sm" ? "pt-3" : props.size === "md" ? "pt-4" : "pt-5";
 
   const onMouseOver = (e: React.MouseEvent<HTMLDivElement>) => {
@@ -64,17 +86,27 @@ export default function Card(props: CardProps) {
       const data = imageData.data;
       let r = 0,
         g = 0,
-        b = 0;
+        b = 0,
+        a = 0;
 
       for (let i = 0; i < data.length; i += 4) {
-        r += data[i];
-        g += data[i + 1];
-        b += data[i + 2];
+        if (data[i + 3] === 0) {
+          r += 255;
+          g += 255;
+          b += 255;
+          a += data[i + 3];
+        } else {
+          r += data[i];
+          g += data[i + 1];
+          b += data[i + 2];
+          a += data[i + 3];
+        }
       }
 
       r = Math.floor(r / (data.length / 4));
       g = Math.floor(g / (data.length / 4));
       b = Math.floor(b / (data.length / 4));
+      a = Math.floor(a / (data.length / 4));
       setBgColor(`rgb(${r},${g},${b})`);
     };
     img.src = props.url;
@@ -82,7 +114,7 @@ export default function Card(props: CardProps) {
 
   useEffect(() => {
     setTimeout(() => {
-      setCardScale(100);
+      setCardScale("scale-100");
     }, 1000);
   }, [cardScale]);
 
@@ -90,7 +122,7 @@ export default function Card(props: CardProps) {
     <div
       ref={cardRef}
       style={{ backgroundColor: `${bgColor}` }}
-      className={`${width} ${height} ${bgColor} rounded-lg shadow-md flex flex-col ${pt} justify-start items-center transition-transform scale-${cardScale}`}
+      className={`${width} ${height} ${bgColor} rounded-lg shadow-md flex flex-col ${pt} justify-start items-center transition-transform ${cardScale}`}
       onMouseMove={onMouseOver}
       onMouseOut={onMouseOut}
     >
